6bec71673281390ec47acaf649d429dd5483454f
Juya <juyaamirm@gmail.com>
2024-08-22 22:44:08 +0200

    checkbox btton is added

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,6 +8,8 @@ const Layout = () => {
   const [taskList, setTaskList] = useState([]);
   const [editIndex, setEditIndex] = useState(null);
   const [editTaskValue, setEditTaskValue] = useState('');
+  const [completedTasks, setCompletedTasks] = useState(new Set());
+  const [warning, setWarning] = useState(false);
 
   useEffect(() => {
     const storedTasks = JSON.parse(localStorage.getItem('to-do-list'));
@@ -25,16 +27,18 @@ const Layout = () => {
    */
   const handleAddTask = (e) => {
     e.preventDefault();
-    if (!newTask || newTask.trim() === '') return;
+    if (!newTask || newTask.trim() === '') return setWarning(true);
     const updatedTaskList = [...taskList, newTask];
     localStorage.setItem('to-do-list', JSON.stringify(updatedTaskList));
     setTaskList(updatedTaskList);
     setNewTask('');
+    setWarning(false);
   };
 
   const handleDeleteAllTasks = () => {
     localStorage.removeItem('to-do-list');
     setTaskList([]);
+    setCompletedTasks(new Set());
   };
 
   /*
@@ -44,6 +48,11 @@ const Layout = () => {
     const updatedTaskList = taskList.filter((_, ind) => ind !== index);
     setTaskList(updatedTaskList);
     localStorage.setItem('to-do-list', JSON.stringify(updatedTaskList));
+    setCompletedTasks((prev) => {
+      const newCompletedTasks = new Set(prev);
+      newCompletedTasks.delete(index);
+      return newCompletedTasks;
+    });
   };
 
   /*
@@ -65,9 +74,25 @@ const Layout = () => {
     setEditTaskValue('');
   };
 
+  /*
+    Handles the checkbox change event.
+   */
+  const handleCheckboxChange = (index) => {
+    setCompletedTasks((prev) => {
+      const newCompletedTasks = new Set(prev);
+      if (newCompletedTasks.has(index)) {
+        newCompletedTasks.delete(index);
+      } else {
+        newCompletedTasks.add(index);
+      }
+      return newCompletedTasks;
+    });
+  };
+
   return (
     <div>
       <h1 className='text-5xl font-bold text-center mt-36'>To-Do List</h1>
+      
       <form onSubmit={handleAddTask} className='border-2 border-gray-600 mx-auto my-6 container p-4 rounded-lg'>
         <div className='flex flex-wrap gap-4'>
           <input
@@ -88,22 +113,29 @@ const Layout = () => {
             Delete All
           </button>
         </div>
+        { warning && <p className='text-red-600 pt-1'><sup>*</sup> Enter your task and then click the add button</p>}
       </form>
-      <ul className='border-2 border-gray-600 mx-auto my-6 container p-4 rounded-lg'>
+      <div className='border-2 border-gray-600 mx-auto my-6 container p-4 rounded-lg'>
         {taskList.map((task, index) => (
-          <li key={index} className='flex gap-4'>
+          <div key={index} className='flex gap-4'>
+            <input
+              type="checkbox"
+              className='border border-red-400 text-red-500 w-10'
+              checked={completedTasks.has(index)}
+              onChange={() => handleCheckboxChange(index)}
+            />
             {editIndex === index ? (
               <input
                 type="text"
                 value={editTaskValue}
-                className='px-2 py-4 border-b border-gray-300 my-2 flex-grow bg-gray-100'
+                className={`px-2 py-4 border-b border-gray-300 my-2 flex-grow ${completedTasks.has(index) ? 'line-through' : ''}`}
                 onChange={(e) => setEditTaskValue(e.target.value)}
               />
             ) : (
               <input
                 type="text"
                 value={task}
-                className='px-2 py-4 border-b border-gray-300 my-2 flex-grow bg-gray-100'
+                className={`px-2 py-4 border-b border-gray-300 my-2 flex-grow bg-gray-100 ${completedTasks.has(index) ? 'line-through' : ''}`}
                 readOnly
               />
             )}
@@ -113,9 +145,9 @@ const Layout = () => {
               <button className='border border-gray-300 py-2 px-4 bg-orange-400 text-white' onClick={() => handleEditTask(index)}>Edit</button>
             )}
             <button className='border border-gray-300 py-2 px-4 bg-red-400 text-white' onClick={() => handleDeleteTask(index)}>Delete</button>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
